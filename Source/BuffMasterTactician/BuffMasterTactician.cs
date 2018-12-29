07b46959eff22a3f77e91b6f2221f42865ad99dd
Mad <github@typ03.de>
2018-12-29 16:47:50 +0100

    add initial codebase

---
+++ Source/BuffMasterTactician/BuffMasterTactician.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using Harmony;
+using BattleTech;
+using System.IO;
+
+
+
+namespace BuffMasterTactician
+{
+    public class BuffMasterTactician
+    {
+        public static string LogPath;
+        public static string ModDirectory;
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            ModDirectory = directory;
+
+            LogPath = Path.Combine(ModDirectory, "BuffMasterTactician.log");
+            File.CreateText(BuffMasterTactician.LogPath);
+
+            var harmony = HarmonyInstance.Create("de.ben.BuffMasterTactician");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(AbstractActor), "OnNewRound", null)]
+    public static class Tactics_Morale_Bonus
+    {
+        public static void Postfix(AbstractActor __instance)
+        {
+            using (var Logger = File.AppendText(BuffMasterTactician.LogPath))
+            {
+                Pilot currentPilot = __instance.GetPilot();
+                if (currentPilot != null && currentPilot.PassiveAbilities.Count > 0)
+                {
+                    for (int i = 0; i < currentPilot.PassiveAbilities.Count; i++)
+                    {
+                        if (currentPilot.PassiveAbilities[i].Def.Description.Id == "AbilityDefT8A")
+                        {
+                            Logger?.WriteLine("[BuffMasterTactician] Pilot is Master Tactician: ModifyMorale(3)");
+
+                            __instance.team.ModifyMorale(3);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Source/BuffMasterTactician/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BuffMasterTactician")]
+[assembly: AssemblyDescription("BuffMasterTactician Mod for Battletech by Harebrained Schemes")]
+[assembly: AssemblyProduct("BuffMasterTactician")]
+[assembly: ComVisible(false)]
+[assembly: Guid("57F70D08-9A46-4760-9DE2-A019A8FD0707")]
+[assembly: AssemblyVersion("1.3.1.*")]
+
